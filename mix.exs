50fadf00409d13b0fda571fb4d7292a45ade7d29
Michael Truog <mjtruog@protonmail.com>
2023-10-26 11:24:25 -0700

    Version 2.0.7.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CGroups.Mixfile do
 
   def project do
     [app: :cgroups,
-     version: "2.0.6",
+     version: "2.0.7",
      language: :erlang,
      erlc_options: [
        {:d, :erlang.list_to_atom('ERLANG_OTP_VERSION_' ++ :erlang.system_info(:otp_release))},
